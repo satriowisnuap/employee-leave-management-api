935e5f5ff90ea0fc2a15e0d902702076267bf5bb
satriowisnu <satriowisnuap@gmail.com>
2026-07-02 19:09:40 +0700

    feat: add leave_requests migration with user association and status management

---
+++ database/migrations/2026_07_02_120454_create_leave_requests_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('leave_requests', function (Blueprint $table) {
+
+            $table->id();
+
+            $table->foreignId('user_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->unsignedInteger('days');
+            $table->text('reason');
+            $table->string('attachment');
+
+            $table->enum('status', [
+                'pending',
+                'approved',
+                'rejected'
+            ])->default('pending');
+
+            $table->foreignId('approved_by')
+                ->nullable()
+                ->constrained('users')
+                ->nullOnDelete();
+
+            $table->timestamp('approved_at')
+                ->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('leave_requests');
+    }
+};
